e5f786adb7987bd377dd250c3037c527eec03cad
REW-Z <leonardzhou@live.com>
2024-05-15 14:21:24 +0800

    DFA update

---
+++ GizboxLang/Src/Data/DFA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gizbox
+{
+    public class DFA
+    {
+        private Dictionary<char, int> table = new Dictionary<char, int>();
+        private int stateCount;
+    }
+}
